10df68d0455ae25b2564b05781d427e748555169
Clemencia Ibanez Padilla <clemenciaibanezpadilla@gmail.com>
2023-11-08 12:12:12 -0300

    commit

---
+++ src/db/models/users.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const usersSchema = new mongoose.Schema({  
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }  
+});
+
+export const usersModel = mongoose.model("Users", usersSchema);
